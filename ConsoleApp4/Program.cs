50a0663772464e50ad50d1e3b3bce43ae3b7b7ba
User <User@WIN-SUEO6K7NQMN>
2022-12-27 23:57:35 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework. example no 3.
+            int n, c;
+            n = 14253;
+            c = 4000000 + 142530 + 2;
+
+            Console.WriteLine(c);
+        }
+    }
+}
